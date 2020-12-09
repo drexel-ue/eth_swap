bd09399fe4e886031d74d7955e616c22fa2b969f
drexel-ue <packappsforyou@gmail.com>
2020-12-09 13:20:33 -0500

    Add and deploy DApp Token and tranfer total supply to EthSwap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,16 @@
+const Token = artifacts.require('Token');
 const EthSwap = artifacts.require('EthSwap');
 
-module.exports = function (deployer) {
-    deployer.deploy(EthSwap);
+module.exports = async function (deployer) {
+    // Deploy Token.
+    await deployer.deploy(Token);
+    const token = await Token.deployed();
+
+    // Deploy EthSwap.
+    await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed();
+
+    // Transfer all tokens to EthSwap.
+    await token.transfer(ethSwap.address, (await token.totalSupply()).toString());
 };
 
