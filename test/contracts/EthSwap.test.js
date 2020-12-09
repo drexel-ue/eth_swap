129552edbc232ec222247b32caf7710423207366
drexel-ue <packappsforyou@gmail.com>
2020-12-09 15:16:01 -0500

    Refactoring

--- test/contracts/EthSwap.test.js
+++ test/contracts/EthSwap.test.js
@@ -7,9 +7,17 @@ require('chai').use(require('chai-as-promised')).should()
 
 contract('EthSwap', (accounts) => {
 
+    let token, ethSwap
+
+    before(async () => {
+        token = await Token.new()
+        ethSwap = await EthSwap.new()
+        totalSupply = (await token.totalSupply()).toString();
+        await token.transfer(ethSwap.address, totalSupply);
+    })
+
     describe('Token deployment', async () => {
         it('contract has a name', async () => {
-            const token = await Token.new()
             const name = await token.name()
 
             assert.equal(name, 'DApp Token')
@@ -18,17 +26,12 @@ contract('EthSwap', (accounts) => {
 
     describe('EthSwap deployment', async () => {
         it('contract has a name', async () => {
-            const ethSwap = await EthSwap.new()
             const name = await ethSwap.name()
 
             assert.equal(name, 'EthSwap Instant Exchange')
         })
 
         it('contract has tokens', async () => {
-            const token = await Token.new()
-            const ethSwap = await EthSwap.new()
-            const totalSupply = (await token.totalSupply()).toString();
-            await token.transfer(ethSwap.address, totalSupply);
             const balance = await token.balanceOf(ethSwap.address)
 
             assert.equal(balance.toString(), totalSupply)
