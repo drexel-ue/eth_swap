b9d8c9a05953865108f07547868f512cfd0a317a
drexel-ue <packappsforyou@gmail.com>
2020-12-17 17:48:29 -0500

    Center and widen

--- src/components/App.js
+++ src/components/App.js
@@ -84,10 +84,10 @@ class App extends Component {
         return (
             <div>
                 <Navbar account={this.state.account} />
-                <div className="container-fluid mt-5">
-                    <div className="row">
-                        <main role="main" className="col-lg-12 d-flex text-center">
-                            <div className="content mr-auto ml-auto">
+                <div className='container-fluid mt-5'>
+                    <div className='row'>
+                        <main role='main' className='col-lg-12 ml-auto mr-auto' style={{ maxWidth: '600px' }}>
+                            <div className='content mr-auto ml-auto'>
                                 {content}
                             </div>
                         </main>
